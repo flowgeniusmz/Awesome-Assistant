47e239e9d731091dbf4a521d58b4dea86af00911
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-02-09 18:05:23 +0000

    Initial

--- Home.py
+++ Home.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from config import pagesetup as ps
 
 
 # 0. Set Page Config
-app_name = "AppName"
+app_name = "FlowGenius AI"
 app_icon = "🌐☁️"
 app_layout = "wide"
 app_sidebar = "collapsed" 
 
 st.set_page_config(page_title=app_name, page_icon=app_icon, layout=app_layout, initial_sidebar_state=app_sidebar)
+
+# Set Page Title
+ps.set_title("FlowGenius", "AI Assistant")
---
+++ pages/7_GPT_VS_GPT.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from config import pagesetup as ps
+
+# 0. Set Page Title
+ps.set_title("AI Assistant", "GPT vs. GPT")
+
+
